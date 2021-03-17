c450f86adac110f97aebe25e0073f1f06718081a
inWork <12>
2021-03-17 17:17:20 +0300

    Add project files.

---
+++ DZ_9_Core_3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+
+namespace DZ_9_Core_3_1
+{
+    class Program
+    {//Создать консольное приложение, которое при старте выводит приветствие, записанное
+     //в настройках приложения (application-scope). Запросить у пользователя имя, возраст и
+     //род деятельности, а затем сохранить данные в настройках. При следующем запуске отобразить
+     //эти сведения. Задать приложению версию и описание.
+        static void Main(string[] args)
+        {
+            //Create the object
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            //make changes
+           // config.AppSettings.Settings["Key2"].Value = "ddd";
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+            var sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+            { 
+            Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+            }
+            Console.ReadLine();
+
+            //save to apply changes
+            
+
+
+        }
+    }
+}
---
+++ DZ_9_Framework/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_9_Framework
+{
+    class Program
+    {//Создать консольное приложение, которое при старте выводит приветствие, записанное
+     //в настройках приложения (application-scope). Запросить у пользователя имя, возраст и
+     //род деятельности, а затем сохранить данные в настройках. При следующем запуске отобразить
+     //эти сведения. Задать приложению версию и описание.
+        static void Main(string[] args)
+        {
+            do
+            {
+                if (CheckUserValue()) GreetingUser();
+                else CreateUserValue();
+
+                Console.WriteLine("1 - Изменить свои данные");
+                Console.WriteLine("2 - Выйти из программы");
+            }
+            while (Next());
+
+
+            bool Next()
+            {
+                char key = Console.ReadKey().KeyChar;
+                if (key == '1')
+                {
+                    Properties.Settings.Default.Reset();
+                    Console.Clear();
+                    return true;
+                }
+                else if (key == '2')
+                {
+                    return false;
+                }
+                return true;
+            }
+            void GreetingUser() => Console.WriteLine($"Полльзователь: {GetUserName()}, {GetUserAge()} лет, место работы: {GetUserWork()}\n");
+            bool CheckUserValue()
+            {
+                if (string.IsNullOrEmpty(Properties.Settings.Default.userName)) return false;
+                return true;
+            }
+            void CreateUserValue()
+            {
+                Console.WriteLine("Введите ваше имя");
+                SaveUserName();
+                Console.WriteLine("Сколько вам лет?");
+                SaveUserAge();
+                Console.WriteLine("Где вы работаете?");
+                SaveUserWork();
+                Console.Clear();
+                GreetingUser();
+            }
+            string GetUserAnswer(int lenghtValue)
+            {
+                while (true)
+                {
+                    string userAnswer = Console.ReadLine();
+                    if (string.IsNullOrEmpty(userAnswer))
+                    {
+                        Console.WriteLine("Значение не может быть пустым, введите заново");
+                    }
+                    else if (userAnswer.Length < lenghtValue)
+                    {
+                        Console.WriteLine("Слишком короткое значение, введите заново");
+                    }
+                    return userAnswer;
+                }
+            }
+            void SaveUserName()
+            {
+                string userName = GetUserAnswer(3);
+                Properties.Settings.Default.userName = userName;
+                Properties.Settings.Default.Save();
+            }
+            void SaveUserAge()
+            {
+                string userAge = GetUserAnswer(1);
+                Properties.Settings.Default.userAge = userAge;
+                Properties.Settings.Default.Save();
+            }
+            void SaveUserWork()
+            {
+                string userWork = GetUserAnswer(3);
+                Properties.Settings.Default.userWork = userWork;
+                Properties.Settings.Default.Save();
+            }
+            string GetUserName() => Properties.Settings.Default.userName;
+            string GetUserAge() => Properties.Settings.Default.userAge;
+            string GetUserWork() => Properties.Settings.Default.userWork;
+        }
+    }
+}
